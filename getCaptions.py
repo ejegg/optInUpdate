862e09dfa995dda47679b9fb6ab3921ccacf27d5
Elliott Eggleston <ejegg@ejegg.com>
2019-03-29 18:08:13 -0500

    Harvest captions, write partial json files

--- getCaptions.py
+++ getCaptions.py
@@ -1,10 +1,15 @@
 import pywikibot
-import dumper
+import urllib.parse
 
 site = pywikibot.Site('commons')
 
+captions = {
+    'en': {},
+    'qqq': {}
+}
 with open('filenames.txt', 'r') as namesfile:
     for name in namesfile:
+        name = name.rstrip()
         imgpage = pywikibot.Page(site, name)
         entityid = 'M' + str(imgpage.pageid)
         wbquery = site._simple_request(
@@ -12,5 +17,21 @@ with open('filenames.txt', 'r') as namesfile:
             ids=entityid)
         result = wbquery.submit()
         entity = result['entities'][entityid]
+        if 'labels' in entity:
+            for language in entity['labels']:
+                if language not in captions:
+                    captions[language] = {}
+                captions[language][entityid] = entity['labels'][language]['value']
+                if language == 'en':
+                    url = 'https://commons.wikimedia.org/wiki/' + urllib.parse.unquote(name)
+                    captions['qqq'][entityid] = 'Photo caption from commons for entity ' + entityid + \
+                        ', can be translated at ' + url
 
-        dumper.dump(result)
+for language in captions:
+    filename = language + '.json'
+    with open(filename, 'w') as i18nfile:
+        for entityid in captions[language]:
+            message = 'fundraisersubscribe-photo-caption-' + entityid
+            value = captions[language][entityid]
+            i18nfile.write('\t"' + message + '": "' + value + '",\n')
+
